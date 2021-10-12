f7b632494e28d59d7ce6a0669c14ff39e3f798b5
Ricardo Lázaro <caffeine305@gmail.com>
2021-10-12 10:48:57 -0500

    2D Walking Animation

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Animator animator;
+    public Rigidbody2D rigidbody;
+
+    Vector2 movement;
+    
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal",movement.x);
+        animator.SetFloat("Vertical",movement.y);
+        animator.SetFloat("Speed",movement.sqrMagnitude);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rigidbody.MovePosition(rigidbody.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
